dafe222c5c28e08b5f4e294b8475a5a52974191a
IgnatiusEzeani <igezeani@yahoo.com>
2022-09-12 13:45:36 +0100

    update setup.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,21 +172,19 @@ else:
 
 def checkbox_container(data):
     st.sidebar.header('What do you want to see')
-    new_data = st.text_input('Enter country Code to add')
-    cols = st.columns(10)
-    if cols[0].button('Add Coutry'):
-        feature_list.append(new_data)
-    if cols[1].button('Select All'):
+    # if st.sidebar.button('Add Coutry'):
+        # feature_list.append(new_data)
+    if st.sidebar.button('Select All'):
         for i in data:
             st.session_state['dynamic_checkbox_' + i] = True
         st.experimental_rerun()
-    if cols[2].button('UnSelect All'):
+    if st.sidebar.button('UnSelect All'):
         for i in data:
             st.session_state['dynamic_checkbox_' + i] = False
         st.experimental_rerun()
     for i in data:
         st.checkbox(i, key='dynamic_checkbox_' + i)
-
+        
 def get_selected_checkboxes():
     return [i.replace('dynamic_checkbox_','') for i in st.session_state.keys() if i.startswith('dynamic_checkbox_') and st.session_state[i]]
 
