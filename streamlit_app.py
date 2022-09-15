9d40302f42d1d2e2ae07a8f4007fa25b33b65029
IgnatiusEzeani <igezeani@yahoo.com>
2022-09-15 23:06:45 +0100

    update setup.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ def read_file(file_source='example'):
     try:
         if file_source=='example':
             fname = st.sidebar.multiselect('Select example data file(s)', sorted([f for f in os.listdir(EXAMPLES_DIR) if f.startswith('Reviews')]))
-            fname = os.path.join(EXAMPLES_DIR, fname)
+            fname = os.path.join(EXAMPLES_DIR, fname[0]) # Todo: Rewrite to process a list of files
         elif file_source=='uploaded':
             uploaded_file = st.sidebar.file_uploader("Upload review data", type=['txt','tsv','xlsx', 'xls'])
             if uploaded_file:
