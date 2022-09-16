8fb4cb3ace51d0309e7dc3d6577115a40d88a2a1
IgnatiusEzeani <igezeani@yahoo.com>
2022-09-16 22:14:55 +0100

    update setup.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,7 +181,7 @@ if status:
         with tab:
             # st.write(data.keys())
             st.write(dir(tabs))
-            tabs.index
+            tabs.__getattribute__
             # if data[tab][0]: #Check whether file was read well okay
                 # analysis = Analysis(data)
                 # if 'feature_list' not in st.session_state.keys():
