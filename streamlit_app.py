0be5c1a3e4f2d4b43182ad67082acea8e170fa66
IgnatiusEzeani <igezeani@yahoo.com>
2022-09-16 23:16:31 +0100

    update setup.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,7 +176,7 @@ else: pass
 status, data = input_data
 if status:
     tabs = st.tabs(data.keys())
-    for i in range(len(data.keys()):
+    for i in range(len(data.keys())):
         st.write(tabs[i])
         # with tab:
             # analysis = Analysis(data)
