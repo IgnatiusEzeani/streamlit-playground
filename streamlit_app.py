4854c97dbf3e872494ac6951e6992224e5d91731
IgnatiusEzeani <igezeani@yahoo.com>
2022-09-29 00:31:18 +0100

    update app file.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -419,17 +419,18 @@ elif task == '🎲 Sentiment Analyzer':
                 if df.empty:
                     st.info('''**NoColumnSelected 🤨**: Please select one or more columns to analyse.''', icon="ℹ️")
                 else:
-                    input_text = ' '.join([' '.join([str(t) for t in list(data[col]) if t not in STOPWORDS]) for col in df])
-                    data = process_sentiments(input_text)
-                    if option == '3 Class Sentiments':
-                        plot_sentiments(data[1], fine_grained=False)
-                    else:
-                        plot_sentiments(data[1])
-                    num_examples = st.slider('Number of example [5 to 20%]',  min_value=5, max_value=20, step=5)
-                    df = pd.DataFrame(data[0], columns =['Review','Polarity', 'Sentiment', 'Subjectivity', 'Category'])
-                    df = df[['Review','Polarity', 'Sentiment']]
-                    df.index = np.arange(1, len(df) + 1)
-                    st.dataframe(df.head(num_examples))
+                    input_text
+                    # input_text = ' '.join([' '.join([str(t) for t in list(data[col]) if t not in STOPWORDS]) for col in df])
+                    # data = process_sentiments(input_text)
+                    # if option == '3 Class Sentiments':
+                        # plot_sentiments(data[1], fine_grained=False)
+                    # else:
+                        # plot_sentiments(data[1])
+                    # num_examples = st.slider('Number of example [5 to 20%]',  min_value=5, max_value=20, step=5)
+                    # df = pd.DataFrame(data[0], columns =['Review','Polarity', 'Sentiment', 'Subjectivity', 'Category'])
+                    # df = df[['Review','Polarity', 'Sentiment']]
+                    # df.index = np.arange(1, len(df) + 1)
+                    # st.dataframe(df.head(num_examples))
 else:
     st.write(task, 'is under construction...')
 
