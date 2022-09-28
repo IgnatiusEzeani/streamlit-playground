c0fe002c9ea60c8ec09a56b78db7477665342c10
IgnatiusEzeani <igezeani@yahoo.com>
2022-09-28 21:21:41 +0100

    update app file.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -201,7 +201,7 @@ def plot_kwic(data, key):
         keyword = st.selectbox('Select a keyword:', topwords).split('(',1)[0].strip()
         window_size = st.slider('Select the window size:', 1, 10, 2)
         maxInsts = st.slider('Maximum number of instances:', 5, 50, 10, 5)
-        col2_lcase = st.checkbox("Lowercase?", key='col2_checkbox')
+        # col2_lcase = st.checkbox("Lowercase?", key='col2_checkbox')
         kwic_instances = get_kwic(input_data, keyword, window_size, maxInsts, col2_lcase)
 
         keyword_analysis = st.radio('Anaysis:', ('Keyword in context', 'Collocation'))
