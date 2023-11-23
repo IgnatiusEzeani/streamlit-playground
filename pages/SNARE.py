3a48f164212aabeeb2ef1d174fa0215b7dcaa22c
Ignatius Ezeani <igezeani@yahoo.com>
2023-11-23 20:03:20 +0000

    modify snare pages

--- pages/SNARE.py
+++ pages/SNARE.py
@@ -9,4 +9,8 @@ sac.buttons([
     sac.ButtonsItem(label='Open an existing project', icon='share-fill', href='https://ant.design/components/button'),
 ], position='left', format_func='title', align='center')
 
-text_data = st.text_area('', placeholder='Copy and paste your data from other applications or websites. You can use tabular (TSV, CSV, DSV) or JSON data.', height=200)
+text_data = st.text_area('', placeholder='Copy and paste your data from other applications or websites. You can use tabular (TSV, CSV, DSV) or JSON data.', height=200, )
+
+sac.buttons([
+    sac.ButtonsItem(label='NEXT', color='#b9ebe2'),
+], position='right', format_func='title', align='center')
