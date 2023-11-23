2324c3d238cf760e0b22ae9e46303cf24bdea047
Ignatius Ezeani <igezeani@yahoo.com>
2023-11-23 18:12:28 +0000

    update requirements.txt

--- pages/SNARE.py
+++ pages/SNARE.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import streamlit_antd_components as sac
 
-text_data = st.text_area('Copy and paste your data from other applications or websites. You can use tabular (TSV, CSV, DSV) or JSON data.', height=200)
+sac.buttons([
+    sac.ButtonsItem(label='button'),
+    sac.ButtonsItem(icon='apple'),
+    sac.ButtonsItem(label='google', icon='google', color='#25C3B0'),
+    sac.ButtonsItem(label='disabled', disabled=True),
+    sac.ButtonsItem(label='link', icon='share-fill', href='https://ant.design/components/button'),
+], position='left', format_func='title', align='center')
+
+text_data = st.text_area(, 'Copy and paste your data from other applications or websites. You can use tabular (TSV, CSV, DSV) or JSON data.', height=200)
