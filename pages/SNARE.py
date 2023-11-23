f24846d1017b9640093b4618c5c71940aa41a13c
Ignatius Ezeani <igezeani@yahoo.com>
2023-11-23 18:25:04 +0000

    modify snare pages

--- pages/SNARE.py
+++ pages/SNARE.py
@@ -2,11 +2,11 @@ import streamlit as st
 import streamlit_antd_components as sac
 
 sac.buttons([
-    sac.ButtonsItem(label='button'),
-    sac.ButtonsItem(icon='apple'),
-    sac.ButtonsItem(label='google', icon='google', color='#25C3B0'),
-    sac.ButtonsItem(label='disabled', disabled=True),
-    sac.ButtonsItem(label='link', icon='share-fill', href='https://ant.design/components/button'),
+    sac.ButtonsItem(label='Paste your corpus', color='#b9ebe2'),
+    sac.ButtonsItem(label='Upload your corpus', color='#b9ebe2'),
+    sac.ButtonsItem(label='Run SPARQL Query', color='#b9ebe2'),
+    sac.ButtonsItem(label='Use Sample Corpus', disabled=True, color='#b9ebe2'),
+    sac.ButtonsItem(label='Open an existing project', icon='share-fill', href='https://ant.design/components/button'),
 ], position='left', format_func='title', align='center')
 
 text_data = st.text_area('','Copy and paste your data from other applications or websites. You can use tabular (TSV, CSV, DSV) or JSON data.', height=200)
