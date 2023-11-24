d519fcaa4b210f04b35d1d6a0dca3fecb2ecd6d7
Ignatius Ezeani <igezeani@yahoo.com>
2023-11-24 22:23:37 +0000

    update snare.py

--- pages/SNARE.py
+++ pages/SNARE.py
@@ -5,8 +5,8 @@ sac.buttons([
     sac.ButtonsItem(label='Open Existing Project', icon='folder2-open',color='#b9ebe2'),
     sac.ButtonsItem(label='Paste your corpus', icon='clipboard', color='#b9ebe2'),
     sac.ButtonsItem(label='Upload your corpus', icon='upload', color='#b9ebe2'),
-    sac.ButtonsItem(label='Use Sample Corpus', color='#b9ebe2'),
-    sac.ButtonsItem(label='Run SPARQL Query', disabled=True, color='#b9ebe2'),
+    sac.ButtonsItem(label='Use Sample Corpus', icon='gift', color='#b9ebe2'),
+    sac.ButtonsItem(label='Run SPARQL Query', icon='cloud', disabled=True, color='#b9ebe2'),
 #    sac.ButtonsItem(label='Open an existing project', icon='share-fill', href='https://ant.design/components/button'),
 ], position='left', format_func='title', align='center')
 
@@ -14,4 +14,4 @@ text_data = st.text_area('', placeholder='Copy and paste your data from other ap
 
 sac.buttons([
     sac.ButtonsItem(label='NEXT', color='#b9ebe2'),
-], position='right', format_func='title', align='right')
+], format_func='title', align='end')
