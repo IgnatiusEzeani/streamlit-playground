dad44c5c8e960ee7925a00f1f80319fcb9cdac9e
Ignatius Ezeani <igezeani@yahoo.com>
2023-12-08 20:49:28 +0000

    modified:   pages/SNARE.py

--- pages/SNARE.py
+++ pages/SNARE.py
@@ -13,12 +13,12 @@ sac.buttons([
     sac.ButtonsItem(label='Open Existing Project', icon='folder2-open',color='#b9ebe2'),
     sac.ButtonsItem(label='Upload your corpus', icon='upload', color='#b9ebe2'),
     sac.ButtonsItem(label='Use Sample Corpus', icon='gift', color='#b9ebe2'),
-    sac.ButtonsItem(label='Run SPARQL Query', icon='cloud', disabled=True, color='#b9ebe2'),
+    sac.ButtonsItem(label='Run SPARQL Query', icon='cloud', color='#b9ebe2', disabled=True),
 #    sac.ButtonsItem(label='Open an existing project', icon='share-fill', href='https://ant.design/components/button'),
-], position='left', format_func='title', align='center')
+    ], position='left', format_func='title', align='center')
 
 text_data = st.text_area('', placeholder='Copy and paste your data from other applications or websites. You can use tabular (TSV, CSV, DSV) or JSON data.', height=200, )
 
 sac.buttons([
     sac.ButtonsItem(label='NEXT', color='#b9ebe2'),
-], format_func='title', align='end')
+    ], format_func='title', align='end')
