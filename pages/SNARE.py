4ed2117700bdb2ef3a12217477c18d4592555e0b
Ignatius Ezeani <igezeani@yahoo.com>
2023-12-11 15:56:25 +0000

    modified:   pages/SNARE.py

--- pages/SNARE.py
+++ pages/SNARE.py
@@ -13,11 +13,11 @@ def do_stuff_on_page_load():
 
 do_stuff_on_page_load()
 
-st.markdown('''<style> ul {list-style-type: none; margin: 0; padding: 1; overflow: hidden; background-color: #adb5ba;}
-            li {float: left;} li a {display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;}
+st.markdown('''<style> ul {list-style-type: none; margin: 0; padding: 14px 16px; overflow: hidden; background-color: #adb5ba;}
+            li.left {float: left;} li.right {float: right;} li a {display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;}
             li a:hover {background-color: #111; } </style>''', unsafe_allow_html=True)
 
-st.markdown("""<ul><li><b>SNARE 1.0</b> Spatial Narrative Representation Environment</li> <li float:right;>Advanced About</li>""", unsafe_allow_html=True)
+st.markdown("""<ul><li class='left'><b>SNARE 1.0</b> Spatial Narrative Representation Environment</li> <li class='right'>Advanced About</li>""", unsafe_allow_html=True)
 
 # st.markdown('### 1. Load your corpus')
 
