90f35c9a9ab6ccbc09c758a0c5ae46904c00f8a0
Ignatius Ezeani <igezeani@yahoo.com>
2023-12-11 20:01:46 +0000

    modified:   pages/SNARE.py

--- pages/SNARE.py
+++ pages/SNARE.py
@@ -72,7 +72,7 @@ else:
     pass
 
 # st.markdown("""<b>📑 SNARE 1.0</b> Spatial Narrative Representation Environment""", unsafe_allow_html=True)
-st.markdown("""<html <style="background-color:#d5dade;"><b>📑 SNARE 1.0</b> Spatial Narrative Representation Environment</html>""", unsafe_allow_html=True)
+st.markdown("""<html style="background-color:#d5dade;"><b>📑 SNARE 1.0</b> Spatial Narrative Representation Environment</html>""", unsafe_allow_html=True)
 # st.markdown("<h4 style='text-align: center; color: #0c0d0c;'>1. Load your corpus</h4>", unsafe_allow_html=True)
 
 
