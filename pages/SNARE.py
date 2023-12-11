09c2ab33419d565672860242a1bb9f995aee0354
Ignatius Ezeani <igezeani@yahoo.com>
2023-12-11 11:21:12 +0000

    modified:   pages/SNARE.py

--- pages/SNARE.py
+++ pages/SNARE.py
@@ -16,7 +16,7 @@ def do_stuff_on_page_load():
     )
 
 do_stuff_on_page_load()
-sac.buttons([
+button = sac.buttons([
     sac.ButtonsItem(label='Paste your corpus', icon='clipboard', color='#b9ebe2'),
     sac.ButtonsItem(label='Open Existing Project', icon='folder2-open',color='#b9ebe2'),
     sac.ButtonsItem(label='Upload your corpus', icon='upload', color='#b9ebe2'),
@@ -24,6 +24,9 @@ sac.buttons([
     sac.ButtonsItem(label='Run SPARQL Query', icon='cloud', color='#b9ebe2', disabled=True),
 #    sac.ButtonsItem(label='Open an existing project', icon='share-fill', href='https://ant.design/components/button'),
     ], position='left', format_func='title', align='center')
+
+st.write("The selected button label is: "+ button)
+
 
 text_data = st.text_area('', placeholder='Copy and paste your data from other applications or websites. You can use tabular (TSV, CSV, DSV) or JSON data.', height=200, )
 
