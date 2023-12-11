4f92d3d2010ef2c62c1a52bb487dec7b502b8297
Ignatius Ezeani <igezeani@yahoo.com>
2023-12-11 19:34:25 +0000

    modified:   pages/SNARE.py

--- pages/SNARE.py
+++ pages/SNARE.py
@@ -48,13 +48,12 @@ with st.container():
 
 button = sac.buttons([
     sac.ButtonsItem(label='Use Sample Corpus', icon='gift', color='#b9ebe2'),
-    sac.ButtonsItem(label=''),
     sac.ButtonsItem(label='Upload your corpus', icon='upload', color='#b9ebe2'),
     sac.ButtonsItem(label='Paste your corpus', icon='clipboard', color='#b9ebe2'),
     sac.ButtonsItem(label='Open Existing Project', icon='folder2-open',color='#b9ebe2'),
     sac.ButtonsItem(label='Run SPARQL Query', icon='cloud', color='#b9ebe2', disabled=True),
     # sac.ButtonsItem(label='Open an existing project', icon='share-fill', href='https://ant.design/components/button'),
-    ], position='left', format_func='title', align='center')
+    ], position='left', format_func='title', align='center', )
 
 
 if button=='Paste your corpus':
