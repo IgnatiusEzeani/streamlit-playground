3e143bb16f475a481cf7c634dc40529d2b9e17e9
Ignatius Ezeani <igezeani@yahoo.com>
2023-12-12 23:22:57 +0000

    modified:   pages/SNARE.py

--- pages/SNARE.py
+++ pages/SNARE.py
@@ -75,3 +75,64 @@ st.markdown("""<b>📑 SNARE 1.0</b> Spatial Narrative Representation Environmen
 
 st.markdown("""<html> <body> <p style="background-color:#d5dade; padding:10x">📑 <b>SNARE 1.0</b> Spatial Narrative Representation Environment</p>
 </body> </html>""", unsafe_allow_html=True)
+
+st.markdown("""<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+.navbar a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.navbar a:hover {
+  background: #f1f1f1;
+  color: black;
+}
+
+.navbar a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+.main {
+  padding: 16px;
+  margin-bottom: 30px;
+}
+</style>
+</head>
+<body>
+
+<div class="navbar">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+</div>
+
+<div class="main">
+  <h1>Bottom Navigation Bar</h1>
+  <p>Some text some text some text.</p>
+</div>
+
+</body>
+</html>
+""", unsafe_allow_html=True)
